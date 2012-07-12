8248129fc8ff76d52c652c3d3e71f2622c3c0969
Devasia Joseph <devasiajoseph@gmail.com>
2012-07-13 01:17:35 +0530

    Added media url

--- directory/settings.py
+++ directory/settings.py
@@ -76,7 +76,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://50.116.21.58/static/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -106,7 +106,7 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '0&amp;t%=o9ki$gw00)a#s#9$_0f9-18kxn8l4t+els)di$*q(_py='
 
-# List of callables that know how to import templates from various sources.
+# List of callables ypysthat know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
